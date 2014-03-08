e9936ea77beb483b58a76d343e43291019b5a501
nguyenducbinh96 <nguyenducbinh0906@gmail.com>
2014-03-09 02:28:43 +0900

    test

--- alarmclock/src/com/tkclock/voice/user_interaction/TkVoiceRecognizerCtrl.java
+++ alarmclock/src/com/tkclock/voice/user_interaction/TkVoiceRecognizerCtrl.java
@@ -70,7 +70,7 @@ public class TkVoiceRecognizerCtrl implements TkVoiceListener.OnListenerResult {
 			m_speaker.release(TASK_PRIORITY_HIGH);
 		}
 	}
-	
+
 	public void start() {
 		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
 		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
